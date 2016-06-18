4b8b67664034b9ca7b5dc4bb4d476551ea7afe73
brianhouse <brian.house@gmail.com>
2016-06-18 11:21:48 -0600

    labels

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from housepy import util, drawing, log
 from colors import colors
 
 LENGTH = 50.0 # meters
-COLORS = {  'contorta':     colors[0], 
+species = {  'contorta':     colors[0], 
             'engelmannii':  colors[1],
             'lasiocarpa':   colors[2],
             'flexilis':     colors[3]
@@ -26,6 +26,10 @@ aspect = ctx.width / ctx.height
 for tree in transect:
     radius_x = tree['size'] / LENGTH
     radius_y = radius_x * aspect
-    ctx.arc(tree['distance'] / LENGTH, tree['size'] / max_size, radius_x, radius_y, fill=COLORS[tree['species']], thickness=0)
+    ctx.arc(tree['distance'] / LENGTH, tree['size'] / max_size, radius_x, radius_y, fill=species[tree['species']], thickness=0)
+
+for i, name in enumerate(species):
+    ctx.line(10 / ctx.width, 1 - ((10 + (i * 10)) / ctx.height), 30 / ctx.width, 1 - ((10 + (i * 10)) / ctx.height), stroke=species[name], thickness=2)
+    ctx.label(35 / ctx.width, 1 - ((14 + (i * 10)) / ctx.height), name.upper(), size=8)    
 
 ctx.output("output/")
